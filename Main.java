7f75982a2a212a722e12c333d985921843a9e231
bwhitmore7 <bryson.whitmore@patelhs.org>
2021-02-22 17:39:32 +0000

    saving

--- Main.java
+++ Main.java
@@ -4,26 +4,14 @@ class Main {
   public static void main(String[] args) {
     Scanner scan = new Scanner(System.in);
   System.out.println("Welcome! when is your birthday?");
-System.out.println("what is the total amount of months in a year?");
-    int totalMonths = scan.nextInt();
-    System.out.println("what is your brith month number");
-    int birthMonth = scan.nextInt();
-    System.out.println("when is your birthday");
-    int birthday = scan.nextInt();
-    System.out.println("how many days are there in a month");
-    int totalDays = scan.nextInt();
-    //message for the 
-    int Month = getMonth(birthMonth, totalMonths);    
-    System.out.println("there will be this many months until your birthday: " + Month);
-    int day = getDay(birthday, totalDays);
-    System.out.println("your birth day is " + day);
-    int result = sum(5);
-    System.out.println(result);
+   int getDay = scan.nextInt();
+    System.out.println("when is your birthday?");
+    int getYear = scan.nextInt();
   }
  //start off your method here. this method is suppost to help you find what day your birthday is on
-static int getMonth(int birthMonth, int totalMonths) {
-    int Month = birthMonth - totalMonths;
-    return Month;
+static int getYear(int year) {
+    int Years = year;
+    return Years;
 }  
 static int getDay(int birthday, int totalDays) {
     int getDay = birthday - totalDays;
